71ff47aaaf82b0a339633bac0fd0381e3bdba0cb
Nikita <surov.n2019@gmail.com>
2020-08-03 10:00:05 +0300

    add custom events, post del function, post add function and change some styles with styled-components

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,37 +1,77 @@
-import React from 'react';
+import React, { Component } from 'react';
 import AppHeader from '../app-header';
 import SearchPanel from '../search-panel';
 import PostStatusFilter from '../post-status-filter';
 import PostList from '../post-list';
 import PostAddForm from '../post-add-form';
 import './css/app.css';
-import './css/app-header.css';
-import './css/index.css';
-import './css/post-add-form.css';
-import './css/post-list.css';
-import './css/post-list-item.css';
-import './css/post-status-filter.css';
-import './css/search-panel.css';
+import './css/index.css'
+import styled from 'styled-components';
 
-const App = () => {
+const AppBlock = styled.div`
+    margin: 0 auto;
+    max-width: 800px;
+`
 
-    const data = [
-        { label: 'Going to learn React', important: true, id: 'wqewq' },
-        { label: 'That is so good', important: false, id: 'dfsa' },
-        { label: 'I need a break...', important: false, id: 'adsfga' }
-    ];
+export default class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [
+                { label: 'Going to learn React', important: true, id: 1 },
+                { label: 'That is so good', important: false, id: 2 },
+                { label: 'I need a break...', important: false, id: 3 }
+            ]
+        };
+        this.deleteItem = this.deleteItem.bind(this);
+        this.addItem = this.addItem.bind(this);
 
-    return (
-        <div className="app">
-            <AppHeader />
-            <div className="search-panel d-flex">
-                <SearchPanel />
-                <PostStatusFilter />
-            </div>
-            <PostList posts={data} />
-            <PostAddForm />
-        </div>
-    )
+        this.maxId = 4;
+
+    }
+
+    deleteItem(id) {
+        this.setState(({ data }) => {
+            const index = data.findIndex(elem => elem.id === id);
+
+            const newArr = [...data.slice(0, index), ...data.slice(index + 1)];
+
+            return {
+                data: newArr
+            }
+        });
+    }
+
+    addItem(body) {
+        const newItem = {
+            label: body,
+            important: false,
+            id: this.maxId++
+        }
+        this.setState(({ data }) => {
+            const newArr = [...data, newItem];
+            return {
+                data: newArr
+            }
+        });
+    }
+
+    render() {
+        return (
+            <AppBlock>
+                <AppHeader />
+                <div className="search-panel d-flex">
+                    <SearchPanel />
+                    <PostStatusFilter />
+                </div>
+                <PostList
+                    posts={this.state.data}
+                    onDelete={this.deleteItem} />
+                <PostAddForm
+                    onAdd={this.addItem} />
+            </AppBlock>
+        )
+    }
+
 }
 
-export default App;
--- src/components/post-list/post-list.js
+++ src/components/post-list/post-list.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import PostListItem from '../post-list-item';
+import { ListGroup } from 'reactstrap';
+import './post-list.css';
 
-const PostList = ({ posts }) => {
+const PostList = ({ posts, onDelete }) => {
 
     const elements = posts.map((item) => {
         const { id, ...itemProps } = item;
@@ -10,15 +12,16 @@ const PostList = ({ posts }) => {
                 <PostListItem
                     // label={item.label}
                     // important={item.important}
-                    {...itemProps} />
+                    {...itemProps}
+                    onDelete={() => onDelete(id)} />
             </li>
         )
     });
 
     return (
-        <ul className="app-list list-group">
+        <ListGroup className="app-list">
             {elements}
-        </ul>
+        </ListGroup>
     )
 }
 
